0e7b343b699e37c0e897effc6c9cb84960e552c7
maatbm <matheusmoraes010@outlook.com.br>
2025-05-28 07:31:13 -0300

    add: JPARepository das entidades curso e aluno criadas

--- src/main/java/modulo5_desafio/repository/CourseRepository.java
+++ src/main/java/modulo5_desafio/repository/CourseRepository.java
@@ -1,4 +1,10 @@
 package modulo5_desafio.repository;
 
-public interface CourseRepository {
+import modulo5_desafio.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/modulo5_desafio/repository/StudentRepository.java
+++ src/main/java/modulo5_desafio/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package modulo5_desafio.repository;
 
-public interface StudentRepository {
+import modulo5_desafio.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
 }
