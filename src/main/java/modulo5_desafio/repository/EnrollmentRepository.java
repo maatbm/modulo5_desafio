3dc46f63db890d6adaa4c9b734c20763687e6d6b
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-05-29 19:56:29 -0300

    Update EnrollmentRepository.java
    refactor: query para relatorio de engajamento

--- src/main/java/modulo5_desafio/repository/EnrollmentRepository.java
+++ src/main/java/modulo5_desafio/repository/EnrollmentRepository.java
@@ -2,14 +2,8 @@ package modulo5_desafio.repository;
 
 import modulo5_desafio.model.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-    @Query("SELECT e FROM Enrollment e WHERE e.course.id = :courseId AND e.enrollmentDate >= :date")
-    List<Enrollment> findByCourseIdAndEnrollmentDateAfter(Long courseId, LocalDate date);
     Optional<Enrollment> findByStudentIdAndCourseId(Long studentId, Long courseId);
 }
