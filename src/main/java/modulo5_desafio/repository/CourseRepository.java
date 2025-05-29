e07559b06ec8999fffd4a266d8856c2b4216f93b
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-05-29 12:59:08 -0300

    Update CourseRepository.java
    add: buscar curso por titulo

--- src/main/java/modulo5_desafio/repository/CourseRepository.java
+++ src/main/java/modulo5_desafio/repository/CourseRepository.java
@@ -4,7 +4,9 @@ import modulo5_desafio.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByTitleContainingIgnoreCase(String title);
+    Optional<Course> findByTitle(String title);
 }
