7ab8ea4b92ccd9036808ce841ebe2d9ea127c8a1
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-05-31 18:14:50 -0300

    Update MenuOption.java
    fix: menu atualizado

--- src/main/java/modulo5_desafio/MenuOption.java
+++ src/main/java/modulo5_desafio/MenuOption.java
@@ -1,15 +1,36 @@
 package modulo5_desafio;
 
 public enum MenuOption {
-    REGISTRAR_ALUNO(1, "Registrar novo aluno",Modulo5DesafioApplication::registerStudent),
-    LISTAR_ALUNOS(2, "Listar alunos ativos",Modulo5DesafioApplication::listActiveStudents),
-    BUSCAR_ALUNO_EMAIL(3,"Buscar aluno por email", Modulo5DesafioApplication::findStudentByEmail),
-    REGISTRAR_CURSO(4,"Registrar novo curso", Modulo5DesafioApplication::registerCourse),
-    LISTAR_CURSOS(5,"Listar cursos ativos", Modulo5DesafioApplication::listActiveCourses),
-    BUSCAR_CURSO_TITULO(6,"Buscar curso por título", Modulo5DesafioApplication::findCourseByTitle),
-    REGISTRAR_MATRICULA(7, "Registrar nova matrícula",Modulo5DesafioApplication::registerEnrollment),
-    LISTAR_MATRICULAS(8, "Listar matrículas ativas",Modulo5DesafioApplication::listEnrollments),
-    RELATORIO_ENGAJAMENTO(9,"Relatório de engajamento", Modulo5DesafioApplication::generateEngagementReport);
+    // Estudantes
+    REGISTER_STUDENT(1, "Registrar aluno", Modulo5DesafioApplication::registerStudent),
+    LIST_ACTIVE_STUDENTS(2, "Listar alunos ativos", Modulo5DesafioApplication::listActiveStudents),
+    LIST_DELETED_STUDENTS(3, "Listar alunos deletados", Modulo5DesafioApplication::listDeletedStudents),
+    LIST_ALL_STUDENTS(4, "Listar todos os alunos", Modulo5DesafioApplication::listAllStudents),
+    DELETE_STUDENT(5, "Deletar aluno", Modulo5DesafioApplication::deleteStudent),
+    RESTORE_STUDENT(6, "Restaurar aluno", Modulo5DesafioApplication::restoreStudent),
+    UPDATE_STUDENT(7, "Atualizar aluno", Modulo5DesafioApplication::updateStudent),
+    FIND_STUDENT_BY_EMAIL(8, "Buscar aluno por email", Modulo5DesafioApplication::findStudentByEmail),
+
+    // Cursos
+    REGISTER_COURSE(9, "Registrar curso", Modulo5DesafioApplication::registerCourse),
+    LIST_ACTIVE_COURSES(10, "Listar cursos ativos", Modulo5DesafioApplication::listActiveCourses),
+    LIST_DELETED_COURSES(11, "Listar cursos deletados", Modulo5DesafioApplication::listDeletedCourses),
+    LIST_ALL_COURSES(12, "Listar todos os cursos", Modulo5DesafioApplication::listAllCourses),
+    DELETE_COURSE(13, "Deletar curso", Modulo5DesafioApplication::deleteCourse),
+    RESTORE_COURSE(14, "Restaurar curso", Modulo5DesafioApplication::restoreCourse),
+    UPDATE_COURSE(15, "Atualizar curso", Modulo5DesafioApplication::updateCourse),
+    FIND_COURSE_BY_TITLE(16, "Buscar curso por título", Modulo5DesafioApplication::findCourseByTitle),
+
+    // Matrículas
+    REGISTER_ENROLLMENT(17, "Registrar matrícula", Modulo5DesafioApplication::registerEnrollment),
+    LIST_ACTIVE_ENROLLMENTS(18, "Listar matrículas ativas", Modulo5DesafioApplication::listActiveEnrollments),
+    LIST_DELETED_ENROLLMENTS(19, "Listar matrículas deletadas", Modulo5DesafioApplication::listDeletedEnrollments),
+    LIST_ALL_ENROLLMENTS(20, "Listar todas as matrículas", Modulo5DesafioApplication::listAllEnrollments),
+    DELETE_ENROLLMENT(21, "Deletar matrícula", Modulo5DesafioApplication::deleteEnrollment),
+    RESTORE_ENROLLMENT(22, "Restaurar matrícula", Modulo5DesafioApplication::restoreEnrollment),
+
+    // Relatórios
+    GENERATE_ENGAGEMENT_REPORT(23, "Gerar relatório de engajamento", Modulo5DesafioApplication::generateEngagementReport);
 
     public final int code;
     public final String label;
