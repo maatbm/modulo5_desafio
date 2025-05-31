5f526d61878f42b24ba9e5f6c332c92a6201ac85
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-05-30 22:01:19 -0300

    Update EnrollmentRepository.java
    add: método para listagem de matriculas ativas

--- src/main/java/modulo5_desafio/repository/EnrollmentRepository.java
+++ src/main/java/modulo5_desafio/repository/EnrollmentRepository.java
@@ -2,8 +2,13 @@ package modulo5_desafio.repository;
 
 import modulo5_desafio.model.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    @NativeQuery(value = "SELECT e.id, e.student_id, e.course_id, e.enrollment_date FROM enrollments e WHERE deleted = false")
+    List<Enrollment> findActiveEnrollments();
     Optional<Enrollment> findByStudentIdAndCourseId(Long studentId, Long courseId);
 }
