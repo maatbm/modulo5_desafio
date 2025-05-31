61ae705df425427d0192afec382380d24d30b96c
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-05-31 12:50:20 -0300

    Update StudentRepository.java
    add: novos métodos de consulta e inserção de dados

--- src/main/java/modulo5_desafio/repository/StudentRepository.java
+++ src/main/java/modulo5_desafio/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package modulo5_desafio.repository;
 
+import jakarta.transaction.Transactional;
 import modulo5_desafio.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.NativeQuery;
 
 import java.util.List;
@@ -10,5 +12,19 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @NativeQuery(value = "SELECT s.id, s.name, s.email, s.birth_date FROM students s WHERE deleted = false")
     List<Student> findActiveStudents();
+
+    @NativeQuery(value = "SELECT s.id, s.name, s.email, s.birth_date FROM students s WHERE deleted = true")
+    List<Student> findInactiveStudents();
+
+    @Modifying
+    @Transactional
+    @NativeQuery(value = "UPDATE students SET deleted = true WHERE id = ?1")
+    boolean softDeleteById(Long id);
+
+    @Modifying
+    @Transactional
+    @NativeQuery(value = "UPDATE students SET name = ?2, email = ?3, birth_date = ?4 WHERE id = ?1")
+    boolean updateStudentById(Long id, String name, String email, String birthDate);
+
     Optional<Student> findByEmail(String email);
 }
