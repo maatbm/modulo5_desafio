f7c85ee1afde934ea94b9a257e04e9613abb35c4
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-05-30 21:34:28 -0300

    Update StudentRepository.java
    add: método para consulta de estudantes ativos

--- src/main/java/modulo5_desafio/repository/StudentRepository.java
+++ src/main/java/modulo5_desafio/repository/StudentRepository.java
@@ -2,9 +2,13 @@ package modulo5_desafio.repository;
 
 import modulo5_desafio.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @NativeQuery(value = "SELECT s.id, s.name, s.email, s.birth_date FROM students s WHERE deleted = false")
+    List<Student> findActiveStudents();
     Optional<Student> findByEmail(String email);
 }
